7ff902812d34a57eef66b5ee7fb8a3b5d75aa135
kkiattikun <52273930+kkiattikun@users.noreply.github.com>
2019-07-11 23:49:20 +0700

    Create practise.py

---
+++ assignment3/practise.py
@@ -0,0 +1,9 @@
+my_list = [20 ,30 ,50]
+print(sum(my_list))
+
+my_tuble = (11 ,22 ,33 ,44 ,55)
+print(my_tuble)
+
+my_sets1 = {1 ,6 ,77 ,9 ,12 ,66 ,99, 5}
+my_sets2 = {2 ,5 ,98 ,12 ,9 ,77 }
+print(my_sets1.intersection(my_sets2))
