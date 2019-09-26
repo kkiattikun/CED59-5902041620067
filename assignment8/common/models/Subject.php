07d00331f8f16daf96facbbb969dd8a392d43bf5
kkiattikun <s5902041620067@email.kmutnb.ac.th>
2019-09-26 15:28:57 +0700

    update model subject

--- assignment8/common/models/Subject.php
+++ assignment8/common/models/Subject.php
@@ -10,6 +10,10 @@ use Yii;
  * @property int $id
  * @property string $name
  * @property string $detail
+ * @property int $create_at
+ * @property int $create_by
+ * @property int $update_at
+ * @property int $update_by
  */
 class Subject extends \yii\db\ActiveRecord
 {
@@ -29,6 +33,7 @@ class Subject extends \yii\db\ActiveRecord
         return [
             [['name'], 'required'],
             [['detail'], 'string'],
+            [['create_at', 'create_by', 'update_at', 'update_by'], 'integer'],
             [['name'], 'string', 'max' => 256],
         ];
     }
@@ -42,6 +47,10 @@ class Subject extends \yii\db\ActiveRecord
             'id' => 'ID',
             'name' => 'Name',
             'detail' => 'Detail',
+            'create_at' => 'Create At',
+            'create_by' => 'Create By',
+            'update_at' => 'Update At',
+            'update_by' => 'Update By',
         ];
     }
 }
